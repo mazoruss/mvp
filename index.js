1c42b613cfe5b938681d407958a943765062f9e3
Jace <chesszhu@gmail.com>
2016-10-17 15:17:02 -0700

    set port

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.set('port', process.env.PORT || 5000)
+
+app.use(express.static(path.join(__dirname, '/client')));
 // app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('hi');
 })
 
-app.listen(5000);
-console.log('listening on port 5000');
+app.listen(app.get('port'));
+console.log('listening');
 
 
 module.exports = app;
